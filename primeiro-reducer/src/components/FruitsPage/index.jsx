4c74f6c6c3314d774300311fe0f7a638c33c2c74
gustavo <go8895806@gmail.com>
2021-09-27 18:21:15 -0300

    feat: added some basics css

--- primeiro-reducer/src/components/FruitsPage/index.jsx
+++ primeiro-reducer/src/components/FruitsPage/index.jsx
@@ -1,13 +1,16 @@
 import { useSelector } from "react-redux";
-
+import './style.css'
 const FruitsPage = () =>{
     const fruits = useSelector((state)=>state.fruits)
     console.log(fruits)
     return(
-        <div>
+        <div className='fruits-container'>
         {
-            fruits.map((fruit)=>(
-                <p key={fruit}>{fruit}</p>
+            fruits.map((item)=>(
+                <div key={ item.fruit } className='fruits'>
+                    <p >{ item.fruit }</p>
+                    <img src={ item.image } alt={item.fruit}/>
+                </div>
             ))
         }
         </div>
