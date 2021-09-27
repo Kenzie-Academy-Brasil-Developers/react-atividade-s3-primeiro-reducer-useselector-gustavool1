6028f4955c6a4a5504ea8c3d6b9d100301069817
gustavo <go8895806@gmail.com>
2021-09-27 18:56:08 -0300

    feat: added an way to put your own fruit

--- primeiro-reducer/src/components/FruitsPage/index.jsx
+++ primeiro-reducer/src/components/FruitsPage/index.jsx
@@ -1,15 +1,34 @@
-import { useSelector } from "react-redux";
+import { TextField, Button } from "@material-ui/core";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFruit } from "../../store/modules/fruits/actions";
 import './style.css'
 const FruitsPage = () =>{
+    const [fruit, setFruit] = useState("")
+    const dispatch = useDispatch()
     const fruits = useSelector((state)=>state.fruits)
-    console.log(fruits)
+    const handleAddFruit = () =>{
+        setFruit("")
+        dispatch(addFruit(fruit))
+        console.log(fruit)
+    }
     return(
+        
         <div className='fruits-container'>
+            <TextField 
+                id='standard-basic' 
+                sx={{m:2}}
+                label='Insira uma fruta'
+                variant='standard'
+                value={fruit}
+                onChange={ (e)=>setFruit(e.target.value) } 
+            />
+                
+            <Button sx={{color:'white'}}onClick={handleAddFruit}>Adicionar Fruta</Button>
         {
             fruits.map((item)=>(
-                <div key={ item.fruit } className='fruits'>
-                    <p >{ item.fruit }</p>
-                    <img src={ item.image } alt={item.fruit}/>
+                <div key={ item } className='fruits'>
+                    <p >{ item }</p>
                 </div>
             ))
         }
--- primeiro-reducer/src/store/modules/fruits/reducer.js
+++ primeiro-reducer/src/store/modules/fruits/reducer.js
@@ -1,18 +1,10 @@
-const initialState = [
-{
-    fruit:'Banana',
-    image:'https://www.bistek.com.br/media/catalog/product/cache/15b2f1f06e1cd470c80b1f3fd7ec8301/3/0/3031276.jpg'
-},
-,{
-    fruit:'Maçã',
-    image:"https://superprix.vteximg.com.br/arquivos/ids/175207-600-600/Maca-Argentina--1-unidade-aprox.-200g-.png?v=636294203590200000"
-}
-,{
-    fruit: 'Morango',
-    image: 'https://www.proativaalimentos.com.br/image/cache/catalog/img_prod/oleo-essencia-morango-100ml-fruta-puro-essencia-massagem-D_NQ_NP_960102-MLB31202671230_062019-F[1]-1000x1000.jpg'
-}]
+const initialState = ['Banana', 'Maçã', 'Morango']
 const reducerFruits = (state = initialState, action) =>{
+    console.log(action)
     switch( action.type ){
+        case "ADD_FRUIT":
+            const { fruit } = action
+            return [...state, fruit] 
         default:
             return state
     }
